a18b370075afe2e6e74f1bbd1495241b34d65bed
Seilce <yasiabalbes@gmail.com>
2025-04-25 21:04:04 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -16,28 +16,10 @@ if (-not (Test-Path $psProfileKey)) {
 }
 
 Set-Location $psProfileKey
-function Set-ConsoleWindow {
-    param(
-        [int]$Width,
-        [int]$Height
-    )
-
-    $WindowSize = $Host.UI.RawUI.WindowSize
-    $WindowSize.Width  = [Math]::Min($Width, $Host.UI.RawUI.BufferSize.Width)
-    $WindowSize.Height = $Height
-
-    try {
-        $Host.UI.RawUI.WindowSize = $WindowSize
-    }
-    catch [System.Management.Automation.SetValueInvocationException] {
-        $Maxvalue = ($_.Exception.Message | Select-String "\d+").Matches[0].Value
-        $WindowSize.Height = $Maxvalue
-        $Host.UI.RawUI.WindowSize = $WindowSize
-    }
-}
-
-# Set the window size to 120 x 30
-Set-ConsoleWindow -Width 120 -Height 30
+$size = $host.UI.RawUI.WindowSize
+$size.Width = 120
+$size.Height = 30
+$host.UI.RawUI.WindowSize = $size
 # Установка шрифта Consolas 16
 New-ItemProperty -Path . -Name FaceName   -Value "Consolas"     -PropertyType String -Force
 New-ItemProperty -Path . -Name FontFamily -Value 0x00000036     -PropertyType DWord  -Force
